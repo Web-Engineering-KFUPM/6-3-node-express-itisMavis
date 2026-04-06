13524e79a2c195b6c86dc4fe3d2ef81f3a06bff5
itisMavis <maiss.a.khalaf704@gmail.com>
2026-04-06 12:57:44 +0300

    TODO5

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 
 
+
 const app = express();
 
 
@@ -13,7 +14,8 @@ import cors from "cors";
 app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+import morgan from "morgan";
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
 
