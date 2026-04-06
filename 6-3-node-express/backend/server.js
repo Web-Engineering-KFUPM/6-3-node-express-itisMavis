fbd58024e5783bde297256f349781e6ff0359cd7
itisMavis <maiss.a.khalaf704@gmail.com>
2026-04-06 13:04:57 +0300

    TODO 6 & 7

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,10 +1,7 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
-
-
-
 const app = express();
-
+import { getRandomQuote } from "./quotes.js";
 
 // TODO 1: Define server port
 const PORT = 3000;
@@ -18,9 +15,17 @@ import morgan from "morgan";
 app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 // TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
