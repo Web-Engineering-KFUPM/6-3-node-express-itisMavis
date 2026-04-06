c6dd3840d59f861bd75a8a02d9b5bee499769d1b
itisMavis <maiss.a.khalaf704@gmail.com>
2026-04-06 12:56:58 +0300

    TODO4

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,6 +1,7 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 
+
 const app = express();
 
 
@@ -8,7 +9,8 @@ const app = express();
 const PORT = 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
 
